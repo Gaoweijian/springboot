80e37e738ab1445b656a4a7a9a1a40a6674d20a2
gaoweijian <gaoweijianComing@163.com>
2020-12-20 10:28:56 +0800

    thymeleaf尝试

--- src/main/java/home/transaction/controller/WebController.java
+++ src/main/java/home/transaction/controller/WebController.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.Map;
+
 
 /**
  * @Author: gao侧耳倾听
@@ -20,8 +23,10 @@ public class WebController {
     private final Logger logger = LoggerFactory.getLogger(WebController.class);
 
     @RequestMapping(value = "/home")
-    public String showPage() {
+    public String showPage(Map<String, Object> map) {
         logger.info("web客户端连接成功...");
+        map.put("version", "1.0.0");
+        map.put("users", Arrays.asList("张三", "李四", "王五"));
         return "home";
     }
 }
